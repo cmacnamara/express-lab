c783705dab340a087102cf252f92e7fd10bf82c6
Chris MacNamara <cpmacnamara@gmail.com>
2023-04-10 18:58:55 -0400

    add students dummy data

---
+++ data/students-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {firstName: "Chris", lastName: "MacNamara", isPassing: true, _id: 653468},
+  {firstName: "Jane", lastName: "Doe", isPassing: false, _id: 583957},
+  {firstName: "Raphael", lastName: "Estevez", isPassing: false, _id: 729579},
+]
+
+export {
+  students
+}
